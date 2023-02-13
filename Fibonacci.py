839a2ae84eeeb135982d5c5f0f7deef5d86174de
Walter Santos <90575788+WalterSantosD@users.noreply.github.com>
2023-02-13 19:16:47 -0300

    Add files via upload

---
+++ Fibonacci.py
@@ -0,0 +1,21 @@
+#soma = lambda x : x > 0 and x**2 + soma(x-1) or 0 
+
+#soma = lambda x : x <= x and or  (x-(x-1))**2 + soma(1+(x-(x-1))) or 0 
+
+#print(soma(6))
+
+#filtro = lambda l : [x for x in l if "e" in x]
+#nomes = ["walter","lucas","pedro","leon"]
+#print (filtro(nomes))
+
+#for x in nomes:
+#    print(x)
+
+
+resultado = chr(ord("a")+1) 
+#print(resultado)
+
+lista = ["a","b"]
+Cifra_codificador = lambda l : [x for x in l ]
+
+print(Cifra_codificador(lista))
