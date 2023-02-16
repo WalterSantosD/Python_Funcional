a9dcdfd8a278d1d3b318f0b036584a19ef2285b3
WalterSantosD <walter.santoskd@gmail.com>
2023-02-16 08:16:59 -0300

    f

--- monad.py
+++
@@ -1,9 +0,0 @@
-def safe_remove(list,i):
-   return ('safe_remove invocado index:',list[i])
-
-lista= [1,2,3,4,5]
-chamada = lambda l,i : l == None and 'Lista vazia' or i > len(l)-1 and 'Index invalido' or safe_remove
-
-print()
-print(chamada(lista,6))
-print(chamada(None,1))
